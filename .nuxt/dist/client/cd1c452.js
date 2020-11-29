9aec6df5d0f0f39230ac78ad1dd453680c013ca4
unknown <jrussellchan2000@gmail.com>
2020-11-30 03:49:30 +0700

    test deploy

--- .nuxt/dist/client/cd1c452.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{473:function(n,e,t){"use strict";t.r(e);var l={},c=t(8),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("ini page buat munculin seluruh category")])}),[],!1,null,null,null);e.default=component.exports}}]);
--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['admin-only'] = require('..\\middleware\\admin-only.js')
-middleware['admin-only'] = middleware['admin-only'].default || middleware['admin-only']
-
-middleware['guest'] = require('..\\middleware\\guest.js')
-middleware['guest'] = middleware['guest'].default || middleware['guest']
-
-export default middleware
