36cbba3829cad16909e55bb6f415e846c9bb904f
unknown <jrussellchan2000@gmail.com>
2020-11-30 01:56:33 +0700

    change starturl pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -156,7 +156,7 @@ export default {
       short_name: 'OG',
       description: 'Choose your gifts, we handle the rest',
       author: 'Jonathan Russell Chan',
-      start_url: 'http://onlinegifting.shop',
+      start_url: '/',
     },
 
     workbox: {
