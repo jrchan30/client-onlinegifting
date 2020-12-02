8148afb578f601c11b766679846dcf9ffaaac8ed
unknown <jrussellchan2000@gmail.com>
2020-12-02 16:51:15 +0700

    debug middleware tries2 use universal cookie middleware

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -1,11 +1,9 @@
 export default async function ({ app, redirect }) {
   // the following look directly for the cookie created by nuxtjs/auth
   // instead of using $auth.loggedIn
-  const user = await app.$cookies.get('auth._token.local')
-  if (user) {
+  const user = await app.$cookies.get('auth._token.laravelSanctum')
+  if (user === false) {
     // let the user see the page
-  } else {
-    // redirect to homepage
     redirect('/')
   }
 }
