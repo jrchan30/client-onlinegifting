9c7c72226f7ec36923953772229d2531de643b69
unknown <jrussellchan2000@gmail.com>
2020-12-03 11:14:46 +0700

    debug auth middlware try1

--- middleware/auth-user.js
+++ middleware/auth-user.js
@@ -2,8 +2,8 @@ export default async function ({ app, redirect }) {
   // the following look directly for the cookie created by nuxtjs/auth
   // instead of using $auth.loggedIn
   const user = await app.$cookies.get('auth._token.laravelSanctum')
+  console.log(user)
   if (user === false) {
-    // let the user see the page
-    redirect('/')
+    redirect('/login')
   }
 }
