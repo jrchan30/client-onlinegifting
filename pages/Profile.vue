ff4fe743023df4cc99b82f2b90d45706518056c3
unknown <jrussellchan2000@gmail.com>
2020-12-05 03:27:06 +0700

    afdsafdasfasdfasdf

--- pages/Profile.vue
+++ pages/Profile.vue
@@ -41,20 +41,22 @@
             </div>
           </div>
         </div>
-        <div v-if="$auth.user.detail" class="container">
-          <CardSkeleton
-            v-if="$auth.user.detail.type !== 'admin'"
-            class="pt-0 pt-lg-5"
-          >
-            <template v-slot:title>
-              <h4 class="font-weight-bold custom-color">Past Transactions</h4>
-              <p class="custom-color">
-                This is your latest past transactions, click on each record to
-                see more details about the transaction
-              </p>
-            </template>
-            <template v-slot:body> </template>
-          </CardSkeleton>
+        <div v-if="$auth.user">
+          <div v-if="$auth.user.detail" class="container">
+            <CardSkeleton
+              v-if="$auth.user.detail.type !== 'admin'"
+              class="pt-0 pt-lg-5"
+            >
+              <template v-slot:title>
+                <h4 class="font-weight-bold custom-color">Past Transactions</h4>
+                <p class="custom-color">
+                  This is your latest past transactions, click on each record to
+                  see more details about the transaction
+                </p>
+              </template>
+              <template v-slot:body> </template>
+            </CardSkeleton>
+          </div>
         </div>
       </div>
     </template>
