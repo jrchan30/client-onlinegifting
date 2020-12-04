178c18ec78e3b4d1fce66815e90f3782ac7626eb
unknown <jrussellchan2000@gmail.com>
2020-12-05 03:19:41 +0700

    test

--- pages/Profile.vue
+++ pages/Profile.vue
@@ -3,7 +3,7 @@
     <template>
       <div class="box-content">
         <div class="container">
-          <div v-if="$auth.user">
+          <div v-if="$auth.user.detail">
             <template v-if="$auth.user.detail.type == 'admin'">
               <vs-alert v-model="admin.active" closable danger class="mb-5">
                 <template #icon>
