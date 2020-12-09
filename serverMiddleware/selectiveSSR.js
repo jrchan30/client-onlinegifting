3f77efac6874a662d1d25d4bcba243d461f40622
unknown <jrussellchan2000@gmail.com>
2020-12-09 23:18:16 +0700

    rollback spa auth

--- serverMiddleware/selectiveSSR.js
+++ serverMiddleware/selectiveSSR.js
@@ -1,8 +1,5 @@
 export default function (req, res, next) {
-  const paths = [
-    '/admin',
-    // '/profile', '/liked-items', '/boxes'
-  ]
+  const paths = ['/admin', '/profile', '/liked-items', '/boxes']
   const url = req.originalUrl
 
   if (paths.includes(req.originalUrl) || url.match('/*/admin*/')) {
