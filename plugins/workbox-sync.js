18dcdf8fd5714fe1acd023147ef9223868197699
unknown <jrussellchan2000@gmail.com>
2020-12-15 20:17:03 +0700

    test background sync'

--- plugins/workbox-sync.js
+++ plugins/workbox-sync.js
@@ -1,6 +1,6 @@
-import { BackgroundSyncPlugin } from 'workbox-background-sync'
-import { registerRoute } from 'workbox-routing'
-import { NetworkOnly } from 'workbox-strategies'
+// import { BackgroundSyncPlugin } from 'workbox-background-sync'
+// import { registerRoute } from 'workbox-routing'
+// import { NetworkOnly } from 'workbox-strategies'
 
 const bgSyncPlugin = new BackgroundSyncPlugin('formQueue', {
   maxRetentionTime: 24 * 60, // Retry for max of 24 Hours (specified in minutes)
