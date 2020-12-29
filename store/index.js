bf6b4f558936c96d210136f9229ed6fbb14f350a
unknown <jrussellchan2000@gmail.com>
2020-12-29 22:49:29 +0700

    add nuxtinit in index catching user on page refresh

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 export const state = () => ({
-  // isLoggedIn: false,
+  isLoggedIn: false,
 })
 
 export const getters = {
@@ -13,26 +13,34 @@ export const getters = {
 }
 
 export const mutations = {
-  // SET_AUTH(state, payload) {
-  //   state.isLoggedIn = payload
-  // },
+  SET_AUTH(state, payload) {
+    state.isLoggedIn = payload
+    state.auth.user = payload.user ?? null
+  },
 }
 
 export const actions = {
-  // async nuxtServerInit({ commit }, { req }) {
-  //   let auth = false
-  //   if (req.headers.cookie) {
-  //     // cookie found
-  //     try {
-  //       // check data user login with cookie
-  //       await this.$axios.$get('/user')
-  //       // server return the data is cookie valid loggedIn is true
-  //       auth = true // set the data auth
-  //     } catch (err) {
-  //       // No valid cookie found
-  //       auth = false
-  //     }
-  //   }
-  //   commit('SET_AUTH', auth) // set state auth
-  // },
+  async nuxtServerInit({ commit }, { req }) {
+    let auth = false
+    let user = null
+    if (req.headers.cookie) {
+      // console.log(req.headers.cookie)
+      // cookie found
+      try {
+        // check data user login with cookie
+        const res = await this.$axios.$get('/user')
+        user = res.data
+        // user = res.user.data
+        console.log('berhasil get')
+        // server return the data is cookie valid loggedIn is true
+        auth = true // set the data auth
+      } catch (err) {
+        console.log('tidak berhasil')
+        // No valid cookie found
+        auth = false
+        user = null
+      }
+    }
+    commit('SET_AUTH', { auth, user }) // set state auth
+  },
 }
