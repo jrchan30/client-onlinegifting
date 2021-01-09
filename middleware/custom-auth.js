b365f55b813818c23dc65445f1a23ca9dd5daa59
unknown <jrussellchan2000@gmail.com>
2021-01-10 01:17:39 +0700

    change custom auth cookies

--- middleware/custom-auth.js
+++ middleware/custom-auth.js
@@ -1,6 +1,9 @@
-export default function ({ app, redirect }) {
-  console.log(app.$auth.loggedIn)
-  if (!app.$auth.loggedIn) {
-    return redirect('/login')
+export default async function ({ app, redirect }) {
+  // the following look directly for the cookie created by nuxtjs/auth
+  // instead of using $auth.loggedIn
+  const user = await app.$cookies.get('auth._token.LaravelSanctum')
+  if (!user) {
+    // redirect to homepage
+    redirect('/login')
   }
 }
