f1d24b71a85620424dc5c1fdc385e8e0e4f4490a
unknown <jrussellchan2000@gmail.com>
2021-01-10 05:08:51 +0700

    custom auth

--- middleware/custom-auth.js
+++ middleware/custom-auth.js
@@ -1,9 +1,26 @@
-export default async function ({ app, redirect }) {
-  // the following look directly for the cookie created by nuxtjs/auth
-  // instead of using $auth.loggedIn
-  const user = await app.$cookies.get('auth._token.laravelSanctum')
-  if (!user) {
-    // redirect to homepage
-    redirect('/register')
+// export default async function ({ app, redirect }) {
+//   // the following look directly for the cookie created by nuxtjs/auth
+//   // instead of using $auth.loggedIn
+// //   const user = await app.$cookies.get('auth._token.laravelSanctum')
+// //   if (!user) {
+// //     // redirect to homepage
+// //     redirect('/register')
+// //   }
+
+// }
+
+// export default function ({ app, redirect }) {
+//   app.router.beforeResolve((to, from, next) => {
+//     if (app.store.getters.isAuthenticated) {
+//       next('/products')
+//     } else {
+//       next('/')
+//     }
+//   })
+// }
+
+export default function ({ store, redirect }) {
+  if (!store.getters.isAuthenticated) {
+    return redirect('/login')
   }
 }
